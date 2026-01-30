7350d4524ef988b9f06611c45f828b2fd3edf5d5
nounfve <rootastgrfvbgh564778hj@gmail.com>
2026-01-31 02:49:21 +0800

    run script

--- scripts/curl_sha256.sh
+++ scripts/curl_sha256.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-url="https://fill-data.papermc.io/v1/objects/4a558a00005d33dafa4c4d5f9e47b3bd47d92311fceccd9c9754ee6b913f8649/paper-1.21.11-100.jar"
+url="https://dl.google.com/dl/android/aosp/akita-bp4a.260105.004.e1-factory-d9bc5fb8.zip"
 
 fname=$(curl --head --remote-name --write-out '%{filename_effective}\n' ${url})
 curl -o ${fname} ${url}
 sha_sum=$(sha256sum ./${fname} | awk '{print $1}')
+echo ${fname}
 echo ${sha_sum}
