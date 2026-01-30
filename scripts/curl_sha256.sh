c9877b47f7fd044af9500f0d8bea03116e996106
nounfve <rootastgrfvbgh564778hj@gmail.com>
2026-01-31 02:42:19 +0800

    run script

--- scripts/curl_sha256.sh
+++ scripts/curl_sha256.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 url="https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-1.21.132.3.zip"
 
-fname=$(curl --head --remote-name --write-out '%{filename_effective}\n' ${url})
-curl -o ${fname} ${url}
+fname=$(curl --http1.1 --head --remote-name --write-out '%{filename_effective}\n' ${url})
+curl --http1.1 -o ${fname} ${url}
 sha_sum=$(sha256sum ./${fname} | awk '{print $1}')
 echo ${sha_sum}
