8671a7fb37d0ac235551af388aa0bbcb68fc9171
nounfve <rootastgrfvbgh564778hj@gmail.com>
2026-01-31 11:43:39 +0800

    fix action

--- lib/scripts/curl_sha256.sh
+++ lib/scripts/curl_sha256.sh
@@ -7,7 +7,7 @@ if [ -z "$url" ]; then
 fi
 
 echo "[Url] ${url}"
-if [ -z "$Mocking"];then
+if [ -n "$Mocking"];then
   fname="akita-bp3a.250905.014-factory-47caa9a7.zip"
   sha_sum="47caa9a7e39670689f1746b7cf6c4770906eb69eff6977c5f03573fa62068697"
   else
