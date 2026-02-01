bc947d96f128a692712dba007d19b6c92d433b1a
nounfve <rootastgrfvbgh564778hj@gmail.com>
2026-02-01 19:17:08 +0800

    issue test

--- main.parse_issue.go
+++ main.parse_issue.go
@@ -25,7 +25,8 @@ func parse_issue() {
 		slog.Error("Recovered from panic", "error", r)
 		fmt.Println("usage: ALT_MAIN=PARSE_ISSUE pixel_boot_img [zip_url]")
 	}()
-	url := os.Args[1]
-	device, build := lib.PARSE_DOWNLOAD_URL(url)
+	input := os.Args[1]
+	fmt.Println(input)
+	device, build := lib.PARSE_DOWNLOAD_URL(input)
 	fmt.Println(device, build)
 }
