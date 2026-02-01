b23df25625dce1a88907b8d9afa4aa52058a0bb4
nounfve <rootastgrfvbgh564778hj@gmail.com>
2026-02-01 19:26:13 +0800

    issue test

--- main.parse_issue.go
+++ main.parse_issue.go
@@ -26,7 +26,9 @@ func parse_issue() {
 		fmt.Println("usage: ALT_MAIN=PARSE_ISSUE pixel_boot_img [zip_url]")
 	}()
 	input := os.Args[1]
+	fmt.Println("=============================")
 	fmt.Println(input)
+	fmt.Println("=============================")
 	device, build := lib.PARSE_DOWNLOAD_URL(input)
 	fmt.Println(device, build)
 }
